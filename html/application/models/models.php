a2ed2e5628c64b63330b923a8cb4cd8921d0bd4f
Alexander <MesaCart@gmail.com>
2012-11-24 15:15:57 -0500

    Added all models - orderNumber is a bit weird and needs to be tested.

--- html/application/models/models.php
+++ html/application/models/models.php
@@ -1,34 +1,114 @@
-
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+    //if ( ! defined('BASEPATH')) exit('No direct script access allowed');
     
-$SERVER = '127.0.0.1';
-$USER = 'shop';
-$PASS = 'password';
-$DATABASE = 'shop';
-
-
-if (!($mylink = mysql_connect( $SERVER, $USER, $PASS)))
-{
-	echo "Could not connect to MySql database.";
-	exit;
-}
-mysql_select_db( $DATABASE );
-
-
-class Email{
+    $SERVER = 'localhost';
+    $USER = 'shop';
+    $PASS = 'enterpasswordhere';
+    $DATABASE = 'shop';
     
-	function get_email($id){
-		$ret_array = array();
-		$sql = "select id,email from EmailTable where id = '$id%'";
-		$result = mysql_query($sql);
-        
-		while($data = mysql_fetch_object($result)){
-			$obj['id'] = $data->id;
-			$obj['email'] = $data->email;
-			$ret_array[] = $obj;
-		}	
-        
-		return $ret_array;
-	}
+    $r = mysql_connect($SERVER, $USER, $PASS);
+    
+    if (!$r)
+    {
+        echo "Could not connect to MySql database.";
+        exit;
+    } else {
+        echo "connection established\n";
     }
+    
+    mysql_select_db( $DATABASE );
+    
+    
+    class Email{
+        
+        function get_email($id){
+            $ret_array = array();
+            $sql = "select id,email from EmailTable where id = '$id%'";
+            $result = mysql_query($sql);
+            
+            while($data = mysql_fetch_object($result)){
+                $obj['id'] = $data->id;
+                $obj['email'] = $data->email;
+                $ret_array[] = $obj;
+            }	
+            
+            return $ret_array;
+    }
+}
+    
+    class Customer{
+        function get_customer($id){
+            $ret_array = array();
+            $sql = "select id,name,email,address from CustomerTable where id = '$id%'";
+            $result = mysql_query($sql);
+            
+            while($data = mysql_fetch_object($result)){
+                $obj['id'] = $data->id;
+                $obj['name'] = $data->name;
+                $obj['email'] = $data->email;
+                $obj['address'] = $data->address;
+                $ret_array[] = $obj;
+            }
+            
+            return $ret_array;
+        }
+        
+    }
+    
+    
+    class News{
+        function get_news($id){
+            $ret_array = array();
+            $sql = "select id,news from NewsTable where id = '$id%'";
+            $result = mysql_query($sql);
+            
+            while($data = mysql_fetch_object($result)){
+                $obj['id'] = $data->id;
+                $obj['news'] = $data->news;
+                $ret_array[] = $obj;
+            }
+            
+            return $ret_array;
+        }
+    }
+    
+    class Product{
+        function get_product($id){
+            $ret_array = array();
+            $sql = "select id,name,description,type,picture,price from ProductTable where id = '$id%'";
+            $result = mysql_query($sql);
+            
+            while($data = mysql_fetch_object($result)){
+                $obj['id'] = $data->id;
+                $obj['name'] = $data->name;
+                $obj['description'] = $data->description;
+                $obj['type'] = $data->type;
+                $obj['picture'] = $data->picture;
+                $obj['price'] = $data->price;
+                $ret_array[] = $obj;
+            }
+            
+            return $ret_array;
+        }
+    }
+    
+    class Order{
+        function get_order($orderNumber){
+            $ret_array = array();
+            $sql = "select orderNumber, productId, quantity, customerId, status from OrdersTable where orderNumber = '$orderNumber%'";
+            $result = mysql_query($sql);
+            
+            if (mysql_num_rows($result) > 0) {
+                while($data = mysql_fetch_assoc($result)) {
+                    $ret_array[] = array('data' => $data);
+                } else {
+                    return $ret_array;
+                }
+            
+            return $ret_array;
+        }
+    }
+    
+    
+        
     
