b843673cf7587f96b5ab417d9d12c9729f10cbcb
Alexander <MesaCart@gmail.com>
2012-11-26 01:36:17 -0500

    stubbed out PUSH, GET, and DELETE for all tables.

--- www/html/api/api.php
+++ www/html/api/api.php
@@ -25,20 +25,34 @@
         $id = array_shift($params_array);
         
         if($method == 'get'){
+            if(empty($id)){
+                $JSON = new json_maker("Email","get_all","");
+                echo $JSON->output;
+            }
+            else{
+                $JSON = new json_maker("Email","get",$id);
+                echo $JSON->output;
+            }
         }
         
         elseif($method == 'put'){
+            array_unshift($params_array, $id);
+            
+            $JSON = new json_maker("Email","put",$params_array);
+            echo $JSON->output;
         }
         
         elseif($method == 'delete'){
+            $JSON = new json_maker("Email","delete",$id);
+            echo $JSON->output;
         }
         
         elseif($method == 'post'){
         }
         
         else{
+            
         }
-        
     }
     
     
@@ -52,7 +66,7 @@
                 echo $JSON->output;
             }
             else{
-                $JSON = new json_maker("Customer","get_customer",$id);
+                $JSON = new json_maker("Customer","get",$id);
                 echo $JSON->output;
             }
         }
@@ -60,12 +74,12 @@
         elseif($method == 'put'){
             array_unshift($params_array, $id);
             
-            $JSON = new json_maker("Customer","put_customer",$params_array);
+            $JSON = new json_maker("Customer","put",$params_array);
             echo $JSON->output;
         }
         
         elseif($method == 'delete'){
-            $JSON = new json_maker("Customer","delete_customer",$id);
+            $JSON = new json_maker("Customer","delete",$id);
             echo $JSON->output;
         }
         
@@ -73,20 +87,110 @@
         }
         
         else{
-        }  
-        
+            
+        }
     }
     
     elseif($resource == 'news'){
+        $id = array_shift($params_array);
         
+        if($method == 'get'){
+            if(empty($id)){
+                $JSON = new json_maker("News","get_all","");
+                echo $JSON->output;
+            }
+            else{
+                $JSON = new json_maker("News","get",$id);
+                echo $JSON->output;
+            }
+        }
+        
+        elseif($method == 'put'){
+            array_unshift($params_array, $id);
+            
+            $JSON = new json_maker("News","put",$params_array);
+            echo $JSON->output;
+        }
+        
+        elseif($method == 'delete'){
+            $JSON = new json_maker("News","delete",$id);
+            echo $JSON->output;
+        }
+        
+        elseif($method == 'post'){
+        }
+        
+        else{
+            
+        }
     }
     
     elseif($resource == 'order'){
+        $id = array_shift($params_array);
         
+        if($method == 'get'){
+            if(empty($id)){
+                $JSON = new json_maker("Order","get_all","");
+                echo $JSON->output;
+            }
+            else{
+                $JSON = new json_maker("Order","get",$id);
+                echo $JSON->output;
+            }
+        }
+        
+        elseif($method == 'put'){
+            array_unshift($params_array, $id);
+            
+            $JSON = new json_maker("Order","put",$params_array);
+            echo $JSON->output;
+        }
+        
+        elseif($method == 'delete'){
+            $JSON = new json_maker("Order","delete",$id);
+            echo $JSON->output;
+        }
+        
+        elseif($method == 'post'){
+        }
+        
+        else{
+            
+        }
     }
     
     elseif($resource == 'product'){
+        $id = array_shift($params_array);
         
+        if($method == 'get'){
+            if(empty($id)){
+                $JSON = new json_maker("Product","get_all","");
+                echo $JSON->output;
+            }
+            else{
+                $JSON = new json_maker("Product","get",$id);
+                echo $JSON->output;
+            }
+        }
+        
+        elseif($method == 'put'){
+            array_unshift($params_array, $id);
+            
+            $JSON = new json_maker("Product","put",$params_array);
+            echo $JSON->output;
+        }
+        
+        elseif($method == 'delete'){
+            $JSON = new json_maker("Product","delete",$id);
+            echo $JSON->output;
+        }
+        
+        elseif($method == 'post'){
+        }
+        
+        else{
+            
+        }
     }
     
     else
