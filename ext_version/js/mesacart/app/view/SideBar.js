82c3cdfa4d97a3dae67a5fe5f140442fc5ca2e62
Alexander <MesaCart@gmail.com>
2012-12-07 03:42:12 -0500

    Facebook should be working

--- ext_version/js/mesacart/app/view/SideBar.js
+++ ext_version/js/mesacart/app/view/SideBar.js
@@ -13,7 +13,11 @@ Ext.define('MC.view.SideBar', {
 				var window = Ext.create('MC.view.CartWindow');
 				window.show();
 			}
-		}
+		},
+                {        html:'<iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%\
+2Fwww.facebook.com%2Fmesacart&amp;width=200&amp;height=590&amp;show_faces=true&amp;colorscheme=ligh\
+t&amp;stream=true&amp;border_color&amp;header=true" scrolling="no" frameborder="0" style="border:no\
+ne; overflow:hidden; width:200px; height:590px;" allowTransparency="true"></iframe>'}
 	]
 	//...attempts to load the widget after the rest of the sidebar has loaded. I belive this is what was explained in the email Alex.	
     //... more configuration ...
